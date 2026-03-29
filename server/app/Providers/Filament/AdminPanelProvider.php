c74492868c9ff7594d0e78fd8fc46d8e0a75da77
Merrick Menongbe <menongbemerrick@gmail.com>
2026-03-29 14:26:14 +0100

    updates

--- server/app/Providers/Filament/AdminPanelProvider.php
+++ server/app/Providers/Filament/AdminPanelProvider.php
@@ -40,9 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->pages([
-                \App\Filament\Pages\Dashboard::class,
-            ])
+            ->pages([])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([])
             ->middleware([
